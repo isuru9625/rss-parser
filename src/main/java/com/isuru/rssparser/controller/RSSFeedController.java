dc87c32af5c585eca54df354080c3974fcf6db84
isuru9625 <isurufernando.16@cse.mrt.ac.lk>
2021-10-30 17:42:32 +0530

    Formatted RESFeedCintroller

--- src/main/java/com/isuru/rssparser/controller/RSSFeedController.java
+++ src/main/java/com/isuru/rssparser/controller/RSSFeedController.java
@@ -1,22 +1,15 @@
 package com.isuru.rssparser.controller;
 
-import com.isuru.rssparser.entities.PollingEvent;
-import com.isuru.rssparser.models.RssFeedEntry;
-import com.isuru.rssparser.repository.IPollingEventRepository;
-import com.isuru.rssparser.service.RSSFeedServiceImpl;
-import com.rometools.rome.feed.synd.SyndEntry;
-import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.io.FeedException;
-import com.rometools.rome.io.SyndFeedInput;
-import com.rometools.rome.io.XmlReader;
+import com.isuru.rssparser.entities.RssFeedEntry;
+import com.isuru.rssparser.service.ILatestItemsService;
+import com.isuru.rssparser.service.IPaginatedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.List;
 
 @RestController
@@ -24,15 +17,25 @@ import java.util.List;
 public class RSSFeedController {
 
     @Autowired
-    RSSFeedServiceImpl rssFeedService;
+    ILatestItemsService latestItemsService;
 
     @Autowired
-    IPollingEventRepository repository;
+    IPaginatedService paginatedService;
 
-    @GetMapping(value = "/test",produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<RssFeedEntry> test(){
-        repository.save(new PollingEvent());
+    @GetMapping(value = "/items",produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<RssFeedEntry> fetchLatestItems()
+    {
+        return latestItemsService.fetchLatestItems();
+    }
 
-        return rssFeedService.getFeedEntries();
+    @GetMapping(value = "/items",produces = {MediaType.APPLICATION_JSON_VALUE}, params = {"page", "size", "sort" })
+    public List<RssFeedEntry> fetchPaginatedItems(@RequestParam("page") int page,
+                                                  @RequestParam("size") int size,
+                                                  @RequestParam("direction") String direction,
+                                                  @RequestParam("sort") String sort)
+    {
+
+        return paginatedService.fetchPaginatedItems(page, size, direction, sort);
+
     }
 }
