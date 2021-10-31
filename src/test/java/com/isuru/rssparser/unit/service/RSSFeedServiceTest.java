98ba4a48b0cce37bca83f74cce57ae51d921f81f
isuru9625 <isurufernando.16@cse.mrt.ac.lk>
2021-10-31 14:14:21 +0530

    Unit Test for transform Method

---
+++ src/test/java/com/isuru/rssparser/unit/service/RSSFeedServiceTest.java
@@ -0,0 +1,50 @@
+package com.isuru.rssparser.unit.service;
+
+import com.isuru.rssparser.entities.PollingEvent;
+import com.isuru.rssparser.entities.RssFeedEntry;
+import com.isuru.rssparser.service.RSSFeedServiceImpl;
+import com.rometools.rome.feed.synd.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManagerFactory;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.mockito.ArgumentMatchers.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RSSFeedServiceTest {
+
+    @Autowired
+    private RSSFeedServiceImpl rssFeedService;
+
+
+    @Test
+    public void testTransformMethod(){
+
+        Date current = new Date();
+        SyndEntry entry = new SyndEntryImpl();
+        entry.setTitle("ABC");
+        SyndContent content = new SyndContentImpl();
+        content.setValue("description");
+        entry.setDescription(content);
+        entry.setPublishedDate( current);
+        entry.setAuthor("author");
+
+        RssFeedEntry rssFeedEntry = rssFeedService.transform( entry, new PollingEvent(10,new Date()) );
+
+        Assert.assertEquals("ABC", rssFeedEntry.getTitle());
+        Assert.assertEquals("author", rssFeedEntry.getAuthor());
+        Assert.assertEquals("description", rssFeedEntry.getDescription());
+        Assert.assertEquals(current, rssFeedEntry.getPublicationDate());
+    }
+}
